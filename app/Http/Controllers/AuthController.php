ae7ec89585570602115b96e72d0aadfd41668fdb
John Rexter Flores <johnrexter.flores@gmail.com>
2021-06-24 11:25:56 +0800

    Implement login api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserLoginRequest;
 
 use App\Models\User;
 
@@ -20,5 +21,28 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'User successfully registered!'
         ], 201);
+    }
+
+    public function login(UserLoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if ( auth()->attempt($credentials) ) {
+            $user = auth()->user();
+
+            $tokenResult = $user->createToken('Login token');
+            $token = $tokenResult->token;
+
+            $token->save();
+
+            return response()->json([
+                'message' => 'Successfully logged in.',
+                'access_token' => $tokenResult->accessToken,
+            ], 201);
+        }
+
+        return response()->json([
+            'message' => 'Invalid credentials.'
+        ], 400);
     }
 }
